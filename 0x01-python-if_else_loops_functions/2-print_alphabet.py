d6be1c58f4ab68cd01193f96c274f24b5f36a9c2
petersudai <psudai@gmail.com>
2023-11-28 05:25:38 -0800

    alpha

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-for i in range(ord('a'), ord('z') + 1):
-    print(chr(i), end="" if i == ord('z') else "")
+for char in range(97, 123):
+    print("{:c}".format(char), end="")
