d2a9b9d85496354aa871c1463fe82803c1517815
petersudai <psudai@gmail.com>
2023-12-01 08:09:06 -0800

    script dynamic!

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,11 +4,12 @@ from sys import argv
 if __name__ == "__main__":
     num_args = len(argv) - 1
     args_list = argv[1:]
-
     plural_suffix = 's' if num_args != 1 else ''
-    ending_punctuation = ':' if num_args != 0 else '.'
 
-    print("{} argument{}{}".format(num_args, plural_suffix, ending_punctuation))
+    print("{} argument{}:".format(num_args, plural_suffix))
 
-    for i, arg in enumerate(arg_list, 1):
+    if num_args > 0:
+        for i, arg in enumerate(arg_list, start=1):
         print("{}: {}".format(i, arg))
+    else:
+        print(".")
