679d375d485036d3c095844a761579409d562570
petersudai <psudai@gmail.com>
2023-12-01 08:14:12 -0800

    script dynamic!

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,7 +9,5 @@ if __name__ == "__main__":
     print("{} argument{}:".format(num_args, plural_suffix))
 
     if num_args > 0:
-        for i, arg in enumerate(arg_list, start=1):
-        print("{}: {}".format(i, arg))
-    else:
-        print(".")
+        for i, arg in enumerate(args_list, start=1):
+            print("{}: {}".format(i, arg))
