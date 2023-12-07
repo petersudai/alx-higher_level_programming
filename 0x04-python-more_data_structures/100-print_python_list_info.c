cca83998f8fb541e1d724173fff3c7944f17ba31
petersudai <psudai@gmail.com>
2023-12-07 12:49:35 -0800

    CPython #0: Python lists

---
+++ 0x04-python-more_data_structures/100-print_python_list_info.c
@@ -0,0 +1,21 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+#include <stdio.h>
+
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t size, i;
+	PyObject *item;
+
+	size = Pylist_Size(p);
+	alloc = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %zd\n", size);
+	printf("[*] Allocated = %zd\n", alloc);
+
+	for (i = 0; i < size; ++i)
+	{
+		item = PyList_GetItem(p, i);
+		printf("Element %zd: %s\n", i, Py_TYPE(item)->tp_name);
+	}
+}
