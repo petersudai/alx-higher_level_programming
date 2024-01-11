2673fc5e18346b0ce192a83b726bc2785b3ad184
petersudai <psudai@gmail.com>
2024-01-11 12:44:28 -0800

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Defines a Student"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
