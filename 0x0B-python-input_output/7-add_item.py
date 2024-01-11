cb4a489dd606d2a6aa7e01c9ae5ed1b83c11e336
petersudai <psudai@gmail.com>
2024-01-11 10:57:39 -0800

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,8 +3,6 @@
 Add item
 """
 import sys
-from os.path import exists
-from json import load, dump
 from save_to_json_file import save_to_json_file
 from load_from_json_file import load_from_json_file
 
@@ -15,14 +13,8 @@ def add_item(args):
     """
     filename = "add_item.json"
 
-    items = []
-    if exists(filename):
-        items = load_from_json_file(filename)
+    items = load_from_json_file(filename)
 
     items.extend(args)
 
     save_to_json_file(items, filename)
-
-if __name__ == "__main__":
-    arguments = sys.argv[1:]
-    add_item(arguments)
