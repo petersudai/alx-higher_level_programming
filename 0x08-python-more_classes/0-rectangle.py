df53cbff924a216de166bbfd723001365c16d385
petersudai <psudai@gmail.com>
2024-01-13 03:35:18 -0800

    Simple rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -5,10 +5,3 @@
 class Rectangle:
     """Empty class representing a rectangle"""
     pass
-
-if __name__ == "__main__":
-    my_rectangle = Rectangle()
-
-    print(type(my_rectangle))
-
-    print(my_rectangke.__dict__)
