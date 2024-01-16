f334c53c47a5ad8a55c89c02c010f3879f23ef0f
petersudai <psudai@gmail.com>
2024-01-16 11:52:36 -0800

    tests

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,3 +14,29 @@ class Square(Rectangle):
     def __str__(self):
         """Returns a string representation of the Square instance"""
         return "[Square] ({}) {}/{} - {}".format(self.id, self._Rectangle__x, self._Rectangle__y, self._Rectangle__width)
+
+    @property
+    def size(self):
+        """Getter for the size attribute"""
+        return self.__width
+
+    @size.setter
+    def size(self, value):
+        """Setter for the size attribute"""
+        self.__width = value
+        self.__height = value
+
+    def update(self, *args, **kwargs):
+        """Update attributes based on *args and **kwargs"""
+        if args:
+            attributes = ["id", "size", "x", "y"]
+
+            for i in range(len(args)):
+                setattr(self, attributes[i], args[i])
+        else:
+            for key, value in kwargs.items():
+                if key == "size":
+                    setattr(self, "_Rectangle__width", value)
+                    setattr(self, "_Rectangle__height", value)
+                else:
+                    setattr(self, key, value)
