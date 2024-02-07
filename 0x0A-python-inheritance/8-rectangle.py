cbf5d7572f0a3f39f731901270202f12f2dc4199
petersudai <psudai@gmail.com>
2024-02-06 22:20:36 -0800

    Full rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -15,4 +15,4 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         """Returns a string representation of the rectangle"""
-        return "[Rectangle] {]/{}".format(self.__width, self.__height)
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -15,7 +15,7 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         """Returns a string representation of the rectangle"""
-        return "[Rectangle] {]/{}".format(self.__width, self.__height)
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
 
     def area(self):
         """Computes and returns the area of the rectangle"""
