92bcd66d5431b6efadd2bdb15cdc86e655c34324
petersudai <psudai@gmail.com>
2024-03-12 17:35:09 -0700

    Sorted occurences

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,14 +2,14 @@
 
 const dict = require('./101-data.js').dict;
 
-let sortedDict = {};
+const sortedDict = {};
 
 for (const userId in dict) {
-    const occurrences = dict[userId];
-    if (!(occurrences in sortedDict)) {
-	sortedDict[occurrences] = [];
-    }
-    sortedDict[occurrences].push(userId);
+  const occurrences = dict[userId];
+  if (!(occurrences in sortedDict)) {
+    sortedDict[occurrences] = [];
+  }
+  sortedDict[occurrences].push(userId);
 }
 
 console.log(sortedDict);
