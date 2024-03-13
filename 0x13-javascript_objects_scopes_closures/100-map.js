196bb3a224c0b1ec8fb97c0c6b6901023d000e89
petersudai <psudai@gmail.com>
2024-03-12 17:20:20 -0700

    Factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const list = require('./100-data').list;
+const list = require('./100-data.js').list;
 
 console.log(list);
 const newList = list.map((value, index) => value * index);
