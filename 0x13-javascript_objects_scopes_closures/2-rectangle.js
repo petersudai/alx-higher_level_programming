969ade97bf4364e1f4d33e4e317e78dc40f22a52
petersudai <psudai@gmail.com>
2024-03-13 02:26:06 -0700

    Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,11 +2,11 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
+    if (isNaN(w) || isNaN(h) || w <= 0 || h <= 0) {
       return {};
     } 
-      this.width = w;
-      this.height = h;
+    this.width = w;
+    this.height = h;
   }
 }
 
