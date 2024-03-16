5c1b6c42c37e75dc9d8e98d6e37ce45b0b03019f
petersudai <psudai@gmail.com>
2024-03-16 09:07:02 -0700

    Delete states

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -11,10 +11,10 @@ if __name__ == "___main__":
     password = sys.argv[2]
     db_name = sys.argv[3]
 
-    ecngine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                             .format(username, password, db_name))
 
-    Base.medadate.create_all(engine)
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
