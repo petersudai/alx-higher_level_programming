2597d512e05b58b2ae7b5fa04cd0bef6232c5590
petersudai <psudai@gmail.com>
2024-03-28 13:17:45 -0700

    POST an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -13,5 +13,4 @@ if __name__ == "__main__":
     email = sys.argv[2]
     data = {'email': email}
     response = requests.post(url, data=data)
-    print("Your email is:", email)
     print(response.text)
