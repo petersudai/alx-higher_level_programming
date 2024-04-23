ffecdc1e2261c013b0215ebce35f2db1ed8bb6e3
petersudai <psudai@gmail.com>
2024-04-23 03:18:51 -0700

    Readme

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+const characterId = '18';
+
+request(apiUrl, (error, response, body) => {
+	if (error) {
+		console.error('Error:', error);
+	} else if (response.statusCode !== 200) {
+		console.error('Error:', response.body);
+	} else {
+		const films = JSON.parse(body).results;
+		const count = films.reduce((total, film) => {
+			if (film.characters.includes(`${apiUrl}${characterId}/`)) {
+				return total + 1;
+			} else {
+				return total;
+			}
+		}, 0);
+		console.log(count);
+	}
+});
