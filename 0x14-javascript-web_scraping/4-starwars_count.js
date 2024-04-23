427e9f222b845598cbcd4b8bfd74c5d2b9a5df2d
petersudai <psudai@gmail.com>
2024-04-23 07:47:17 -0700

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,7 +3,7 @@
 const request = require('request');
 
 request(process.argv[2], function (error, response, body) {
-  if (!error) {
+  if (!error && response.statusCode === 200) {
     const films = JSON.parse(body).results;
     let count = 0;
     films.forEach(film => {
