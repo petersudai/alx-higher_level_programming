c8e2075fb1af7ea336900a19849e25e10bb224f4
petersudai <psudai@gmail.com>
2024-04-23 03:21:13 -0700

    Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,9 +4,9 @@ const request = require('request');
 const url = process.argv[2];
 
 request.get(url, (error, response) => {
-	if (error) {
-		console.error(error);
-		return;
-	}
-	console.log(`code: ${response.statusCode}`);
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log(`code: ${response.statusCode}`);
 });
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -6,12 +6,12 @@ const movieID = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
 
 request(url, (error, response, body) => {
-	if (error) {
-		console.error('Error:', error);
-	} else if (response.statusCode !== 200) {
-		console.error('Error:', response.body);
-	} else {
-		const movie = JSON.parse(body);
-		console.log(movie.title);
-	}
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Error:', response.body);
+  } else {
+    const movie = JSON.parse(body);
+    console.log(movie.title);
+  }
 });
