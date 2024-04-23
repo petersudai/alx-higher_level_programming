8a4eafc6d6144f84139f4934b8570dcd1954f646
petersudai <psudai@gmail.com>
2024-04-23 03:27:43 -0700

    Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,10 +5,10 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-fs.writeFile(filePath, content, 'utf-8', (err) => {
+fs.writeFile(filePath, content, 'utf8', (err) => {
   if (err) {
     console.error(err);
-    return;
+  } else {
+  console.log(`Content has been written to ${filePath}`);
   }
-  console.log('The file has been saved!');
 });
--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,19 +6,19 @@ const apiUrl = process.argv[2];
 const characterId = '18';
 
 request(apiUrl, (error, response, body) => {
-	if (error) {
-		console.error('Error:', error);
-	} else if (response.statusCode !== 200) {
-		console.error('Error:', response.body);
-	} else {
-		const films = JSON.parse(body).results;
-		const count = films.reduce((total, film) => {
-			if (film.characters.includes(`${apiUrl}${characterId}/`)) {
-				return total + 1;
-			} else {
-				return total;
-			}
-		}, 0);
-		console.log(count);
-	}
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Error:', response.body);
+  } else {
+    const films = JSON.parse(body).results;
+    const count = films.reduce((total, film) => {
+      if (film.characters.includes(`${apiUrl}${characterId}/`)) {
+        return total + 1;
+      } else {
+        return total;
+      }
+    }, 0);
+    console.log(count);
+  }
 });
