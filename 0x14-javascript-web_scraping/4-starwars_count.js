6c803a069cbb1c5839777a06bf257fe258c73d53
petersudai <psudai@gmail.com>
2024-04-23 04:21:20 -0700

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,11 +5,14 @@ const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (!error) {
     const films = JSON.parse(body).results;
-	  const characterID = '18';
 
-	  const filteredMovies = films.filter(movie => movie.characters.some(character => character.endsWith(characterId)));
-
-	  console.log(filteredMovies.length);
+	  let count = 0;
+	  films.forEach(film => {
+		  if (film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+			  count++;
+		  }
+	  });
+	  console.log(count);
   } else {
 	  console.error(error);
   }
