cc242052e6e13bbdb64db34d0aafb1143492f29f
petersudai <psudai@gmail.com>
2024-04-23 06:41:10 -0700

    How many completed?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,18 +5,18 @@ const request = require('request');
 const apiUrl = process.argv[2];
 
 request(apiUrl, function (error, response, body) {
-	if (!error && response.statusCode === 200) {
-		const tasks = JSON.parse(body);
+  if (!error && response.statusCode === 200) {
+    const tasks = JSON.parse(body);
 
-		const completedTasksByUser = tasks.reduce((acc, task) => {
-			if (task.completed) {
-				acc[task.userId] = (acc[task.userId] || 0) + 1;
-			}
-			return acc;
-		}, {});
+    const completedTasksByUser = tasks.reduce((acc, task) => {
+      if (task.completed) {
+        acc[task.userId] = (acc[task.userId] || 0) + 1;
+      }
+      return acc;
+    }, {});
 
-		console.log(completedTasksByUser);
-	} else {
-		console.error(error);
-	}
+    console.log(completedTasksByUser);
+  } else {
+    console.error(error);
+  }
 });
